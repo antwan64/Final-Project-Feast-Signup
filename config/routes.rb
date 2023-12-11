19432e1629a2a30b7472abb5d2b3b49bce651949
antwan64 <146212683+antwan64@users.noreply.github.com>
2023-12-11 05:08:37 +0000

    Sign Up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   # Routes for the Event resource:
 
   root to: "events#index"
